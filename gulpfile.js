b62b415670eb49b831f7e57632243c4a04426e53
Chris Rogers <chrxr@outlook.com>
2014-10-19 21:14:24 +0100

    tablet styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
 
-gulp.task('sass', function () {
+gulp.task('base', function () {
     gulp.src('./flowpatrolcore/static/flowpatrolcore/css/scss/base.scss')
         .pipe(sass())
         .pipe(gulp.dest('./flowpatrolcore/static/flowpatrolcore/css/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('tablet', function () {
+    gulp.src('./flowpatrolcore/static/flowpatrolcore/css/scss/tablet.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./flowpatrolcore/static/flowpatrolcore/css/'));
+});
+
+gulp.task('default', ['base', 'tablet']);
